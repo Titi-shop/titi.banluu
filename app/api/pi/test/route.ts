3775423963a5c1fd668007d2b8d1ae720292d227
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-10-17 22:11:18 +0700

    Update route.ts

--- app/api/pi/test/route.ts
+++ app/api/pi/test/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const API_KEY = process.env.bkw1u0q3zody4kiynkwbd00emdt1st1z9lpnfrvi637nxnvavsggimacdnfampbb;
+    const API_KEY = process.env.PI_API_KEY;
     const res = await fetch("https://api.minepi.com/v2/payments/test", {
       headers: { Authorization: `Key ${API_KEY}` },
     });
