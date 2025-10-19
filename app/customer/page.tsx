e90ff4b8318e7f98b57bc8a3c09d6ba4b8f4beee
Titi-shop <nguyenminhduc99991991@gmail.com>
2025-10-19 21:09:41 +0700

    Update page.tsx

--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useLanguage } from "../context/LanguageContext";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
