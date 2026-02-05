96bc0e015e3135d4b475867e014501b455d85b04
Titi-shop <226829233+Titi-shop@users.noreply.github.com>
2026-02-05 17:42:51 +0700

    Update page.tsx

--- app/customer/review/page.tsx
+++ app/customer/review/page.tsx
@@ -284,9 +284,6 @@ export default function CustomerReviewPage() {
           </div>
         )}
       </section>
-
-      {/* FLOAT BUTTON */}
-      <button className="fixed bottom-6 right-6 w-12 h-12 rounded-full bg-orange-500 shadow-lg" />
     </main>
   );
 }
