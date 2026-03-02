f03717625fa992215ae42d1d3dd548edbb96e49a
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-03-03 00:23:05 +0700

    Create validEmailDomains.ts

---
+++ data/validEmailDomains.ts
@@ -0,0 +1,26 @@
+// data/validEmailDomains.ts
+
+export const validEmailDomains = [
+  "gmail.com",
+  "googlemail.com",
+  "yahoo.com",
+  "hotmail.com",
+  "outlook.com",
+  "live.com",
+  "icloud.com",
+  "protonmail.com",
+  "yandex.com",
+  "mail.com",
+];
+
+export const blockedEmailDomains = [
+  "10minutemail.com",
+  "temp-mail.org",
+  "mailinator.com",
+  "guerrillamail.com",
+  "fakemail.net",
+  "getnada.com",
+  "throwawaymail.com",
+  "trashmail.com",
+  "dispostable.com",
+];
